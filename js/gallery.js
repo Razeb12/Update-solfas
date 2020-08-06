70d6d7c2b95c4b6887937b34fde0eec888f11bab
Razeb Enyi <enyirazeb@gmail.com>
2020-08-06 15:43:43 +0100

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,20 @@
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+ // MDB Lightbox Init
+ $(function () {
+     $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+ });
